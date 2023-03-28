abd6f7c86b077de2b2d5e7753b564ea1c36ade06
haido86 <thithuhai.do@gmail.com>
2023-03-28 19:04:49 +0300

    Frontend: fix Home page for products

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -23,10 +23,13 @@ function Home() {
         </button>
       )}
       <div className="p-1 m-5 grid grid-cols-3 gap-y-10 gap-x-4 transition duration-150 sm:grid-cols-5">
-        {products &&
-          products.filteredProductArr.map((product) => (
-            <ProductCard key={product.id} product={product} addToCart={addToCart} />
-          ))}
+        {products?.filteredProductArr.length > 0
+          ? products.filteredProductArr.map((product) => (
+              <ProductCard key={product.id} product={product} addToCart={addToCart} />
+            ))
+          : products.items.map((product) => (
+              <ProductCard key={product.id} product={product} addToCart={addToCart} />
+            ))}
       </div>
     </div>
   )
