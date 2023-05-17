a46efd9e2f760d134277ca4c70ccecad99088ad0
haido86 <thithuhai.do@gmail.com>
2023-05-17 22:40:36 +0300

    Frontend: styling header

--- src/components/ExtraBar.tsx
+++ src/components/ExtraBar.tsx
@@ -4,46 +4,44 @@ import { useDispatch, useSelector } from 'react-redux'
 import { filteredProductsAction } from '../slices/products/productsSlice'
 import { AppDispatch, RootState } from '../store/store'
 
-function ExtraBar({ className }: { className: string }) {
+function ExtraBar() {
   const { products } = useSelector((state: RootState) => state)
   const dispatch = useDispatch<AppDispatch>()
   const filterCategory = products.selectedCategory
 
   return (
-    <div className={className}>
-      <div className="mx-10 justify-around px-2 py-1 flex flex-col sm:flex-row">
-        {products.filteredProductArr.length === 0 ||
-        products.filteredProductArr.length === products.items.length ||
-        products.selectedCategory === null ? (
-          products?.categories.map((category) => (
-            <div key={category.id}>
-              <div className="flex m-1">
-                <button
-                  onClick={() => {
-                    dispatch(filteredProductsAction(category.name))
-                  }}
-                  className="bg-gray-200 rounded-full px-3 py-1 capitalize"
-                  key={category.id}>
-                  {category.name.toLowerCase()}
-                </button>
-              </div>
-            </div>
-          ))
-        ) : (
-          <div className="flex">
-            <button
-              className="mr-20 hover:bg-gray-200 rounded-full p-2"
-              onClick={() => {
-                dispatch(filteredProductsAction(''))
-              }}>
-              <IoArrowBackOutline size={20} />
-            </button>
-            <div className="bg-gray-200 rounded-full px-3 py-1 capitalize">
-              {filterCategory?.name.toLowerCase()}
+    <div className="mx-10 justify-around px-2 py-1 flex flex-col sm:flex-row">
+      {products.filteredProductArr.length === 0 ||
+      products.filteredProductArr.length === products.items.length ||
+      products.selectedCategory === null ? (
+        products?.categories.map((category) => (
+          <div key={category.id}>
+            <div className="flex mb-1">
+              <button
+                onClick={() => {
+                  dispatch(filteredProductsAction(category.name))
+                }}
+                className="bg-gray-200 rounded-full px-3 py-1 capitalize"
+                key={category.id}>
+                {category.name.toLowerCase()}
+              </button>
             </div>
           </div>
-        )}
-      </div>
+        ))
+      ) : (
+        <div className="flex">
+          <button
+            className="mr-20 hover:bg-gray-200 rounded-full p-2"
+            onClick={() => {
+              dispatch(filteredProductsAction(''))
+            }}>
+            <IoArrowBackOutline size={20} />
+          </button>
+          <div className="bg-gray-200 rounded-full px-3 py-1 capitalize">
+            {filterCategory?.name.toLowerCase()}
+          </div>
+        </div>
+      )}
     </div>
   )
 }
